4967c62c9c87e12aa0f5166172335674b2dfc567
Parshikov, Vladimir <vladimirus@gmail.com>
2016-07-21 16:32:25 +0100

    refactored

--- src/main/scala/com/test/TestApp.scala
+++ src/main/scala/com/test/TestApp.scala
@@ -3,10 +3,11 @@ package com.test
 import org.apache.spark.{SparkContext, SparkConf}
 
 object TestApp extends App {
-  val textFile = "src/main/resources/lotto.txt"
   val conf = new SparkConf().setAppName("My App").setMaster("local[2]")
   val sc = new SparkContext(conf)
-  val logData = sc.textFile(textFile, 2).cache()
+
+  val logData = sc.textFile("src/main/resources/lotto.txt", 10).cache()
+
   val numAs = logData.filter(line => line.contains("Guinevere")).count()
   val numBs = logData.filter(line => line.contains("Arthur")).count()
   println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
